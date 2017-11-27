a5a3364fe7120dd4b4f73a1612009373c0893a3b
Kamil S Jaroň <376090@mail.muni.cz>
2017-11-27 13:29:29 +0100

    today final try

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,13 +1,13 @@
 
     /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
     var disqus_config = function () {
-    this.page.url = '{{ site.disqus_shortname }}';  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = '{{ page.url }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    this.page.url = '{{ page.url | absolute_url }}';
+    this.page.identifier = '{{ page.url | absolute_url }}';
     };
 
     (function() {
     var d = document, s = d.createElement('script');
-    s.src = 'https://https-kamilsjaron-github-io.disqus.com/embed.js';
+    s.src = 'https://{{ site.disqus.shortname }}.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
     })();
